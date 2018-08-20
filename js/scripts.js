1b94972df8dbe07a63799184a7b895a484484b69
Meria Thomas and Steve Santos <meria@>
2018-08-20 11:44:32 -0700

    add files and code without prototype

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,10 @@ $(document).ready(function() {
     $("#list").append("<li><input type='checkbox' name='to-do'> " + input+ "</li>");
     $("#list li").unbind('click').click(function() {
       $(this).remove();
+      console.log($(this).text());
+      $("#completedList").append("<li>"+$(this).text()+"<li>");
     });
-    
+
 });
 
 });
